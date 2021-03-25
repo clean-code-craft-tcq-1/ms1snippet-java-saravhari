4ce785b5d98dfaf3cc58d379a22f0ef85717f7bb
Saravanan S <selvaraj.saravanan@in.bosch.com>
2021-03-25 19:03:05 +0530

    Tasks Completed

--- src/main/java/sensorval/SensorValidator.java
+++ src/main/java/sensorval/SensorValidator.java
@@ -2,30 +2,23 @@ package sensorval;
 
 import java.util.List;
 
-public class SensorValidator 
-{
-    public static boolean _give_me_a_good_name(double value, double nextValue, double maxDelta) {
-        if(nextValue - value > maxDelta) {
-            return false;
-        }
-        return true;
-    }
-    public static boolean validateSOCreadings(List<Double> values) {
-        int lastButOneIndex = values.size() - 1;
-        for(int i = 0; i < lastButOneIndex; i++) {
-            if(!_give_me_a_good_name(values.get(i), values.get(i + 1), 0.05)) {
-            return false;
-            }
-        }
-        return true;
-    }
-    public static boolean validateCurrentreadings(List<Double> values) {
-        int lastButOneIndex = values.size() - 1;
-        for(int i = 0; i < lastButOneIndex; i++) {
-            if(!_give_me_a_good_name(values.get(i), values.get(i + 1), 0.1)) {
-            return false;
-            }
-        }
-        return true;
-    }
+public class SensorValidator {
+	public static boolean checkIfDiffValuesExceedsMaxDelta(double value, double nextValue, double maxDelta) {
+		if (nextValue - value > maxDelta) {
+			return false;
+		}
+		return true;
+	}
+
+	public static boolean validateReadings(List<Double> values, double maxDelta) {
+		if (values == null)
+			return false;
+		int lastButOneIndex = values.size() - 1;
+		for (int i = 0; i < lastButOneIndex; i++) {
+			if (!checkIfDiffValuesExceedsMaxDelta(values.get(i), values.get(i + 1), maxDelta)) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
